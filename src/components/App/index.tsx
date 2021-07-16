7c0a8b93e5747c5c7e5f24e5cc609ff04bed6e94
Cainã Brazil <cainabrazil@gmail.com>
2021-07-16 10:53:31 +0100

    Renamed job, added key to child

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -38,7 +38,7 @@ function App() {
                 transaction={t}
               />
               :
-              <Skeleton />)
+              <Skeleton key={`skltn-${i}`}/>)
           }
           )
       }
